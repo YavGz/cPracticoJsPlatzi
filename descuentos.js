82a75650ace0e73fc0d1b6189834c71a18a94e83
Javier Gutierrez (Yav) <gutierrez.alcantara.javier@gmail.com>
2021-07-09 00:46:11 -0500

    Proyecto de porcentajes y descuentos termiando

--- descuentos.js
+++ descuentos.js
@@ -15,24 +15,30 @@ const coupons = [
 ];
 
 // inputs  de cupones 
-const inputCoupon = document.getElementById("InputCoupon");
-const couponValue = inputCoupon.value
-const userCoupon = coupons.find(isCouponValueValid);
 
-const isCouponValueValid = function (coupon) {
-  return coupon.name === couponValue;
-};  
 
-if (!userCoupon) {
-  alert("El cupón " + userCouponValue + "no es válido");
-} else {
-  const descuento = userCoupon.discount;
-  const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+const discountCoupon = () => {
   
-  const resultP = document.getElementById("ResultP");
-  resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
-}
-  
+  const isCouponValueValid = function (coupon) {
+    return coupon.name === couponValue;
+  };
+
+  const priceInput = document.getElementById("OriginalPrice");
+  const priceValue = priceInput.value;
+  const inputCoupon = document.getElementById("InputCoupon");
+  const couponValue = inputCoupon.value;
+  const userCoupon = coupons.find(isCouponValueValid);
+
+  if (!userCoupon) {
+    alert("El cupón " + couponValue + " no es válido");
+  } else {
+    const descuento = userCoupon.discount;
+    const precioConDescuento = priceWithOff(priceValue, descuento);
+    
+    const resultP = document.getElementById("priceResult");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  } 
+}  
 
 // Formula para generar precio total
 function priceWithOff(price, off) { 
@@ -49,10 +55,11 @@ const calcPriceOffer = () => {
   const total = priceWithOff(priceValue, discountValue);
   // input para respuesta
   const priceResult = document.getElementById("priceResult");
-  priceResult.innerHTML = `El precio a pagar con el descuento es: $${total} MXN`;
+  priceResult.innerText = `El precio a pagar con el descuento es: $${total} MXN`;
 }
 
 const cleanInputs = () => {
+  const couponInput = document.getElementById("InputCoupon").value = "";
   const priceInput = document.getElementById("OriginalPrice").value = "";
   const discountInput = document.getElementById("Discount").value = "";
   const priceResult = document.getElementById("priceResult");
