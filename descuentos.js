b54dfd10e64818e1b82e84a93f5838da47b0ab19
Javier Gutierrez (Yav) <gutierrez.alcantara.javier@gmail.com>
2021-07-08 20:09:58 -0500

    Pre de codigo de cupones

--- descuentos.js
+++ descuentos.js
@@ -1,3 +1,39 @@
+// coupons
+const coupons = [
+  {
+      name: "Off Day",
+      discount: 15,
+  },
+  {
+      name: "Off Year",
+      discount: 30,
+  },
+  {
+      name: "Secret off",
+      discount: 25,
+  },
+];
+
+// inputs  de cupones 
+const inputCoupon = document.getElementById("InputCoupon");
+const couponValue = inputCoupon.value
+const userCoupon = coupons.find(isCouponValueValid);
+
+const isCouponValueValid = function (coupon) {
+  return coupon.name === couponValue;
+};  
+
+if (!userCoupon) {
+  alert("El cupón " + userCouponValue + "no es válido");
+} else {
+  const descuento = userCoupon.discount;
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+  
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
+  
+
 // Formula para generar precio total
 function priceWithOff(price, off) { 
   const priceOff = (price * (100 - off)) / 100 ;
