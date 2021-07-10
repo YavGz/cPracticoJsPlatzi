fb701287946978767f7961bd1c97ad3bf037cb01
Javier Gutierrez (Yav) <gutierrez.alcantara.javier@gmail.com>
2021-07-09 20:26:21 -0500

    Logica para calcular la mediana

--- promedio.js
+++ promedio.js
@@ -13,6 +13,39 @@ const calcMediaAritmetica = (list) => {
   const promedioList = sumlist / list.length;
   return promedioList;
 }
-
 // call to function
 // calcMediaAritmetica([n,n,n,n])
+
+
+// Cal mediana 
+
+const listMediana = [
+  100,
+  200,
+  300,
+  500,
+  600,
+  // 400000,
+];
+
+const mitadLista = parseInt(listMediana.length / 2) ;
+
+const esPar = (number) =>{
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let mediana;
+
+if (esPar(listMediana.length)) {
+  const element1 =  listMediana[mitadLista - 1];
+  const element2 = listMediana[mitadLista];
+  const promedioMediana = calcMediaAritmetica([element1, element2]);
+  mediana = promedioMediana;
+} else {
+  const promedioMediana = listMediana[mitadLista]
+  mediana = promedioMediana;
+};
