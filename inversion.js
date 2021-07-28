6beea1c803c57d634f095a54f7f0bfbb56ccac21
Javier Gutierrez (Yav) <gutierrez.alcantara.javier@gmail.com>
2021-07-27 20:20:30 -0500

    Preparando funcion para limpiar inputs

--- inversion.js
+++ inversion.js
@@ -1,38 +1,67 @@
-let saldoInicial = 500;
-let tazaDeCrecimientoAnual = 8;
-let periodoDeInversion = 12;
-let interesMensual;
-let gananciaMensual;
+
 let totalInteresfijo;
 let totalInteresCompuesto;
 
-const CalcInteresMensual = () => {
-  const saldo = saldoInicial * ((tazaDeCrecimientoAnual / 100) /12);
-  interesMensual = saldo;
-  return interesMensual;
-};
 
-const CalGananciaFija = () => {
+const CalcIntFijo = () => {
+
+  let InputSalary = document.getElementById("InputSalary");
+  let saldoInicial = parseInt( InputSalary.value);
+  let tazaDeinteres = document.getElementById("InputTaza");
+  let tazaDeCrecimientoAnual = parseInt(tazaDeinteres.value);
+  let tiempo = document.getElementById("Tiempo");
+  let tiempoInversion = tiempo.value;
+  let periodoDeInversion = parseInt(tiempoInversion);
+
+  const interes = saldoInicial * ((tazaDeCrecimientoAnual / 100) /12);
+  interesMensual = interes;
+
   const ganancia = interesMensual * periodoDeInversion;
   gananciaMensual = ganancia;
-  return gananciaMensual
-};
 
-const CalcGananciaIntFijo = () => {
-  CalcInteresMensual();
-  CalGananciaFija();
   let suma = saldoInicial + gananciaMensual;
   totalInteresfijo = suma;
+
+  const alert = document.getElementById("AlertResult");
+  alert.className = "alert alert-primary";
+  alert.innerText = `Tus Ganancias potenciales con interes fijo son $${totalInteresfijo}`;
+  
   return totalInteresfijo;
+
 };
 
-function investmentWithCompoundInterest() 
-{
-  let capital = Number(saldoInicial);
-  let tiempo = Number(periodoDeInversion);
-  let tasaDeInteres = Number((tazaDeCrecimientoAnual / 100)/12) ;
-  // Calculamos el monto final con interes compuesto
-  let montoFinal = capital * (1 + tasaDeInteres) ** tiempo;
-  return Number(montoFinal);
-}
+const CalIntCompuesto = () => {
 
+  let InputSalary = document.getElementById("InputSalary");
+  let saldoInicial = parseInt( InputSalary.value);
+  let tazaDeinteres = document.getElementById("InputTaza");
+  let tazaDeCrecimientoAnual = parseInt(tazaDeinteres.value);
+  let tiempo = document.getElementById("Tiempo");
+  let periodoDeInversion = parseInt(tiempo.value);
+
+  let interes = (tazaDeCrecimientoAnual / 100 ) / 12;
+
+  let gananciaCompuesta = saldoInicial * (1 + interes ) ** periodoDeInversion;
+
+  totalInteresCompuesto = gananciaCompuesta;
+
+  const alert2 = document.getElementById("AlertResult2");
+  alert2.className = "alert alert-primary";
+  alert2.innerText = `Tus Ganancias potenciales con interes compuesto son: $${totalInteresCompuesto}`
+
+  return totalInteresCompuesto;
+
+};
+
+const limpiarImputs = () => {
+  const InputSalary = document.getElementById("InputSalary").value = "";
+  const interes = document.getElementById("InputTaza").value= "";
+  const tiempo = document.getElementById("Tiempo").value = "";
+
+  const alert = document.getElementById("AlertResult");
+  
+  alert.className += "d-none"
+  
+  const alert2 = document.getElementById("AlertResult2");
+
+}
